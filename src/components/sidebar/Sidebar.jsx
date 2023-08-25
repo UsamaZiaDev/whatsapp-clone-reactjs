012d7df105667efbb09de2e156ccd827aadc3a76
usamaZia <usamazia@addo.ai>
2023-08-25 16:59:33 +0500

    sidebar-search-ui-design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,8 +2,14 @@ import UserInfoCard from '../common/UserInfoCard'
 
 const Sidebar = () => {
   return (
-    <div className="bg-primary vh-100 p-3">
+    <div className="bg-primary vh-100">
       <UserInfoCard/>
+
+      <div className="sidebar-chat-search p-3">
+        <input type="text" class="form-control form-control-md p-" placeholder="search..." />
+      </div>
+
+
     </div>
   )
 }
