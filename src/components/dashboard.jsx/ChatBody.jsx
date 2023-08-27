d54829aaea166596b5b44e029d923f01eb25f82d
usamaZia <usamazia@addo.ai>
2023-08-27 12:17:00 +0500

    create-user-chat-box-topbar-ui-design

--- src/components/dashboard.jsx/ChatBody.jsx
+++ src/components/dashboard.jsx/ChatBody.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+
+import {MdCall} from "react-icons/md"
+import {BsThreeDotsVertical} from "react-icons/bs"
+import {AiOutlineSearch} from "react-icons/ai"
+import {GoDotFill} from "react-icons/go"
+
+import Profile2 from "./../../assets/images/profile-2.jpg"
 
 const ChatBody = () => {
   return (
-    <div className="bg-success vh-100">ChatBody</div>
+    <div className="bg-primary border-start border-dim vh-100">
+      
+    {/* chtabox-topbar */}
+     <div className="bg-dark user-info-card p-4 d-flex align-items-center  border-bottom  border-dim">
+        <img src={Profile2} className="rounded-circle img-60 border-success border-3 border"/>
+        <div className="text-light con ms-3 g-danger w-100 d-flex justify-content-between">
+              <div>
+                <h4 className="mb-0"> Frontend Developer </h4>
+                <GoDotFill className="text-success"/> <span  className="text-dim"> online </span>
+              </div>
+              <p className="fs-4"> 
+                <AiOutlineSearch className="fs-3"/>
+                <MdCall className="mx-3"/> 
+                <BsThreeDotsVertical/>
+              </p>
+        </div>
+      </div>  
+      
+      
+    </div>
   )
 }
 
